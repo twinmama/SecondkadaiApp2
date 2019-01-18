e9d1b20c3c9169a5f989b262e1e6792b25cd79d0
twinmama <twintwinmama2525@gmail.com>
2019-01-18 14:30:35 +0900

    Initial Commit

---
+++ SecondKadaiApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp2
+//
+//  Created by Ashiwa Mami on 2019/01/18.
+//  Copyright © 2019 twinmama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
